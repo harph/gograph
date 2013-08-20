f1e590e0b6fdc4d0d111aff51dee3a4f3017f508
Harrington Joseph <harrington.joseph@demandmedia.com>
2013-08-20 16:21:33 -0700

    Added existing (initial)  code base

---
+++ graph.go
@@ -0,0 +1,134 @@
+package gograph
+
+import (
+    "fmt"
+)
+
+
+type nodeValue interface {}
+
+func getNodeKey(nv nodeValue) string {
+    return fmt.Sprintf("%b", nv)
+}
+
+
+type node struct {
+    key string // Mapping/identifier node value
+    Value nodeValue // Actual value of the node
+    OutgoingArcs map[string] node
+    IncomingArcs map[string] node
+}
+
+func newNode(key string, nv nodeValue) *node {
+    return &node{
+        key: key,
+        Value: nv,
+        OutgoingArcs: make(map[string] node),
+        IncomingArcs: make(map[string] node),
+    }
+}
+
+/*
+Returns true if the arc is created, otherwide it returns false because the arc,
+already exists. It also returns false if the arc points to itself.
+*/
+func (n *node) addArcTo(nodeTo node) bool {
+    nodeToKey := nodeTo.key
+    nodeFromKey := n.key
+    if nodeToKey == nodeFromKey {
+        return false
+    }
+    _, ok := n.OutgoingArcs[nodeToKey]
+    if !ok {
+        n.OutgoingArcs[nodeToKey] = nodeTo
+        nodeTo.IncomingArcs[nodeFromKey] = *n
+    }
+    return !ok
+}
+
+
+// Represents a graph data structure.
+type graph struct {
+    nodeMap map[string] node // Keeps track of the nodes
+}
+
+// Creates, initializes and returns a graph strcutre.
+func NewGraph() *graph {
+    return &graph{
+        nodeMap: make(map[string] node),
+    }
+}
+
+/*
+Check if the node "nv" exists in the graph.
+Returns true if so else returns false.
+*/
+func (g *graph) HasNode(nv nodeValue) bool{
+    key := getNodeKey(nv)
+    _, ok := g.nodeMap[key]
+    return ok
+}
+
+/*
+Adds a node to the graph if it doesn't exist. If the node is added it returns
+true, otherwise it returns false indicating that the node has not been added
+because it already existed.
+*/
+func (g *graph) AddNode(nv nodeValue) bool {
+    ok := g.HasNode(nv)
+    if !ok {
+        key := getNodeKey(nv)
+        g.nodeMap[key] = *newNode(key, nv)
+    }
+    return !ok
+}
+
+/*
+Gets the node that match the "nodeValue". Returns the node if it exists,
+otherwise it returns nil
+*/
+func (g *graph) getNode(nv nodeValue) *node {
+    nodeKey := getNodeKey(nv)
+    n, ok := g.nodeMap[nodeKey]
+    if ok {
+        return &n
+    }
+    return nil
+}
+
+/*
+Creates an arc (unidirectional) from "nodeFromValue" to "nodeToValue". It
+returns true if the arc has been created and false if the arc already existed
+or if "nodeFromValue" is equals to "nodeToValue".
+*/
+func (g *graph) AddArc(nodeFromValue, nodeToValue nodeValue) bool {
+    g.AddNode(nodeFromValue)
+    g.AddNode(nodeToValue)
+    nodeFrom := g.getNode(nodeFromValue)
+    nodeTo := g.getNode(nodeToValue)
+    return nodeFrom.addArcTo(*nodeTo)
+}
+
+/*
+Creates an edge (bidrectional) between "node1Value" and "node2Value". It
+returns a boolean array with the values true if the arc was created or false
+if it already existed. i.e: {true, false} means that the arc from "node1Value"
+to "node2Value" has been created and that the arc from "node2Value" to
+"node1Value" has not been created because it already existed. It also returns
+{false, false} if both node values are the same.
+*/
+func (g *graph) AddEdge(node1Value, node2Value nodeValue) [2]bool{
+    g.AddNode(node1Value)
+    g.AddNode(node2Value)
+    node1 := g.getNode(node1Value)
+    node2 := g.getNode(node2Value)
+    return [2]bool{
+        node1.addArcTo(*node2),
+        node2.addArcTo(*node1),
+    }
+}
+
+
+func (graph *graph) PrintGraph() {
+    fmt.Printf("%v\n", graph.nodeMap)
+}
---
+++ graph_test.go
@@ -0,0 +1,196 @@
+package gograph
+
+
+import (
+    "testing"
+)
+
+
+type testValue interface {}
+
+
+// AddNode test.
+func TestAddNode(t *testing.T) {
+    graph := NewGraph()
+    dummyValue1 := new(testValue)
+    type dummyStruct struct{
+        X int
+        Y int
+    }
+    dummyValue2 := &dummyStruct{10, 20}
+    testCases := []struct {
+        input testValue
+        output bool
+    }{
+        {1, true}, // An int node
+        {1, false}, // An existing node
+        {3.14, true}, // A float node
+        {"foo", true}, // A string node
+        {"foo", false}, // A existing string node
+        {nil, true}, // A nil node
+        {nil, false}, // An existing nil node
+        {[]int{1, 2, 3}, true}, // An array node
+        {[]int{1, 2, 3}, false}, // An existing array node
+        {dummyValue1, true}, // An interface instance
+        {dummyValue1, false}, // An existing interface
+        {dummyValue2, true}, // A struct
+        {dummyValue2, false}, // An existing struct
+    }
+    for _, testCase := range testCases {
+        added := graph.AddNode(testCase.input)
+        if added != testCase.output {
+            t.Errorf(
+                "graph.AddNode(%#v) returned \"%t\" when \"%t\" was expected.\n",
+                testCase.input, added, testCase.output,
+            )
+        }
+    }
+}
+
+// HasNode test.
+func TestHasNode(t *testing.T) {
+    graph := NewGraph();
+    dummyValue1 := new(testValue)
+    type dummyStruct struct{
+        X int
+        Y int
+    }
+    dummyValue2 := &dummyStruct{10, 20}
+    dummyValue3 := []int{1, 2, 3}
+    graph.AddNode(1);
+    graph.AddNode(3.14);
+    graph.AddNode("foo");
+    graph.AddNode(nil);
+    graph.AddNode(dummyValue1);
+    graph.AddNode(dummyValue3);
+    testCases := []struct {
+        input testValue
+        output bool
+    }{
+        {1, true},
+        {2, false},
+        {3.14, true},
+        {2.71, false},
+        {"foo", true},
+        {"bar", false},
+        {nil, true},
+        {dummyValue1, true},
+        {dummyValue2, false},
+        {dummyValue3, true},
+    }
+    for _, testCase := range testCases {
+        hasIt := graph.HasNode(testCase.input)
+        if hasIt != testCase.output {
+            t.Errorf(
+                "graph.HasNode(%#v) returned \"%t\" when \"%t\" was expected.\n",
+                testCase.input, hasIt, testCase.output,
+            )
+        }
+    }
+}
+
+// HasArc test.
+func TestHasArc(t *testing.T) {
+
+}
+
+// HasEdge test.
+func TestHasEdge(t *testing.T) {
+
+}
+
+// AddArc test.
+func TestAddArc(t *testing.T) {
+    graph := NewGraph()
+    type dummyStruct struct{
+        X int
+        Y int
+    }
+    dummyValue := new(testValue)
+    dummyArray := []int{1, 2, 3}
+    testCases := []struct{
+        inputNodeFrom testValue
+        inputNodeTo testValue
+        outputAddedArc bool
+    }{
+        {"A", "A", false},
+        {"A", "B", true},
+        {"A", dummyArray, true},
+        {dummyValue, dummyValue, false},
+        {dummyValue, "A", true},
+        {dummyValue, nil, true},
+        {dummyArray, dummyArray, false},
+        {dummyArray, "B", true},
+        {nil, nil, false},
+        {nil, dummyValue, true},
+        {nil, dummyArray, true},
+        {"A", "B", false},
+        {"A", dummyArray, false},
+        {"A", nil, true},
+    }
+    for _, testCase := range testCases {
+        nodeFrom := testCase.inputNodeFrom
+        nodeTo := testCase.inputNodeTo
+        added := graph.AddArc(nodeFrom, nodeTo)
+        expectedOuput := testCase.outputAddedArc
+        // First check if both nodes exists in the graph
+        if !graph.HasNode(nodeFrom) || !graph.HasNode(nodeTo) {
+            t.Errorf(
+                "After executing graph.addArc(%#v, %#v) " + 
+                "at least one of the nodes is not in the graph.",
+                nodeFrom, nodeTo,
+            )
+        } else if (added != expectedOuput) {
+            t.Errorf(
+                "graph.addArc(%#v, %#v) returned " +
+                "\"%t\" when \"%t\" was expected",
+                nodeFrom, nodeTo, added, expectedOuput,
+            )
+        }
+    }
+}
+
+// AddEdge test.
+func TestAddEdge(t *testing.T) {
+    graph := NewGraph()
+    type dummyStruct struct{
+        X int
+        Y int
+    }
+    dummyValue := new(testValue)
+    dummyArray := []int{1, 2, 3}
+    testCases := []struct{
+        inputNode1 testValue
+        inputNode2 testValue
+        outputAddedEdges [2]bool
+    }{
+        {"A", "B", [2]bool{true, true}}, // An unexisting edge
+        {"A", "B", [2]bool{false, false}}, // An existing edge
+        {"B", "A", [2]bool{false, false}}, // An existing edge
+        {"A", dummyArray, [2]bool{true, true}}, // An unexisting edge
+        {dummyValue, dummyArray, [2]bool{true, true}}, // An unexisting edge
+        {dummyArray, dummyValue, [2]bool{false, false}}, // An existing edge
+        {dummyArray, dummyArray, [2]bool{false, false}}, // Edge to itself
+        {nil, dummyArray, [2]bool{true, true}}, // An unexisting edge
+    }
+    for _, testCase := range testCases {
+        node1 := testCase.inputNode1
+        node2 := testCase.inputNode2
+        added := graph.AddEdge(node1, node2)
+        expectedOuput := testCase.outputAddedEdges
+        // First check if both nodes exists in the graph
+        if !graph.HasNode(node1) || !graph.HasNode(node2) {
+            t.Errorf(
+                "After executing graph.addEdge(%#v, %#v) " + 
+                "at least one of the nodes is not in the graph.",
+                node1, node2,
+            )
+        } else if (added != expectedOuput) {
+            t.Errorf(
+                "graph.addEdge(%#v, %#v) returned " +
+                "\"%v\" when \"%v\" was expected",
+                node1, node2, added, expectedOuput,
+            )
+        }
+    }
+}
